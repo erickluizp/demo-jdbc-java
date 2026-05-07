44e4dd28230a1bc6f719b2e16120b30385ffbb3e
Erick Luiz <elxfp14@gmail.com>
2026-05-07 08:03:35 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
         System.out.println(obj);
 
         Seller seller = new Seller(1, "Rick", "rick@gmail.com", LocalDate.of(2005, 4, 14), 2500.0, new Department(1, "Books"));
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
